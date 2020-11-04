493dd00aad57555c4699f3be0fcb90f5d6664f0c
dopplix <jmlambda@naver.com>
2020-11-04 14:36:57 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
+import QR from 'qrcode';
 
 function App() {
+  const qrcanvas = useRef(null);
+  const input = useRef(null);
+  const [text, setText] = useState("Make QR");
+
+  useEffect(()=>{
+    if (qrcanvas !== null && qrcanvas.current !== null) {
+      QR.toCanvas(qrcanvas.current, text);
+    }
+  },[text])
+
+  const onButtonClicked = useCallback(()=>{
+    if(input.current.value){
+      setText(input.current.value);
+    }
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <body>
+        <form onSubmit={(e) => { e.preventDefault(); onButtonClicked() }}>
+          <input ref={input}></input>
+          <button>Make</button>
+        </form>
+        <canvas ref={qrcanvas}></canvas>
+      </body>
+    </html>
   );
 }
 
